de95f8a356548010ddd7f0fad4da42edd68bc661
Jon Short <JonShort@users.noreply.github.com>
2018-06-27 14:52:50 +0100

    Further work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+if (!process.env.TEST_EMAIL_ADDRESS) {
+  throw new Error('Please set the TEST_EMAIL_ADDRESS environment variable');
+}
+
+const path = process.env.TEST_EMAIL_PATH || './src/testing-email-path.js';
+const userEmail = process.env.TEST_EMAIL_ADDRESS;
+
 const text = 'Some text here that has been written to a file';
 
 fs.writeFile('src/writtenFile.txt', text, err => {
