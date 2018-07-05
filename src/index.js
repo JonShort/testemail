0ea5be614965912e780d56dd1a7dacbd19208852
Jon Short <JonShort@users.noreply.github.com>
2018-07-05 16:29:26 +0100

    Added more functionality

--- src/index.js
+++ src/index.js
@@ -25,19 +25,27 @@ const generateTestEmail = async () => {
     const userEmailSplit = userEmail.split('@');
     const currentDate = Date.now();
 
-    const text = `${userEmailSplit[0]}+${currentDate}@${userEmailSplit[1]}`;
-    const dashArray = new Array(text.length).fill('-');
+    const generatedEmail = `${userEmailSplit[0]}+${currentDate}@${
+      userEmailSplit[1]
+    }`;
+    const dateOfGeneration = Date(currentDate);
+
+    const dashArray = new Array(generatedEmail.length).fill('-');
     const dashes = dashArray.join('');
 
-    fs.writeFile(path, text, err => {
+    const textToWrite = `Provided email: ${userEmail}
+Generated email: ${generatedEmail}
+Date generated: ${dateOfGeneration}`;
+
+    fs.writeFile(path, textToWrite, err => {
       if (err) throw err;
 
       console.log(
-        '\x1b[36m%s\x1b[0m',
-        `\r\nEmail generated on ${Date(currentDate)}`
+        '\x1b[32m%s\x1b[0m',
+        `\r\nEmail generated on ${dateOfGeneration}`
       );
       console.log('\x1b[32m%s\x1b[0m', dashes);
-      console.log(text);
+      console.log(generatedEmail);
       console.log('\x1b[32m%s\x1b[0m', dashes);
     });
   } catch (e) {
